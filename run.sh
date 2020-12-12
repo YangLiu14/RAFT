b539d2c95ccb6b64a44b2e2815ae3d1fc17c9e7a
YangLiu14 <lander14@outlook.com>
2020-12-12 17:44:45 +0100

    trivials

--- run.sh
+++ run.sh
@@ -14,12 +14,12 @@ python gen_opt_flow.py --model /mnt/raid/davech2y/liuyang/model_weights/RAFT/raf
 
 python gen_opt_flow.py --model /mnt/raid/davech2y/liuyang/model_weights/RAFT/raft-sintel.pth \
                        --path /mnt/raid/davech2y/liuyang/data/TAO/frames/val/ \
-                       --outdir /mnt/raid/davech2y/liuyang/Optical_Flow/TaoVal_RAFT_sintel/ \
-                       --datasrc Charades
+                       --outdir /mnt/raid/davech2y/liuyang/Optical_Flow/TaoVal_RAFT_sintel_downscaled/ \
+                       --datasrc YFCC100M
 
 python gen_opt_flow.py --model /storage/slurm/liuyang/model_weights/RAFT/raft-sintel.pth \
                        --path /storage/slurm/liuyang/data/TAO/TAO_VAL/val/ \
-                       --outdir /storage/slurm/liuyang/Optical_Flow/TaoVal_RAFT_sintel/ \
+                       --outdir /storage/slurm/liuyang/Optical_Flow/TaoVal_RAFT_sintel_downscaled/ \
                        --datasrc ArgoVerse
 
 python opt_flow_warp.py --prop_dir /mnt/raid/davech2y/liuyang/TAO_eval/TAO_VAL_Proposals/Panoptic_Cas_R101_NMSoff_forTracking/boxNMS/_objectness/ \
